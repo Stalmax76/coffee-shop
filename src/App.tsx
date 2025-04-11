9590762a7c1d4bac48b242c01fb1faf9a5ed2684
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2025-04-11 12:18:16 +0200

    start

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Button from './components/Button';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>The exemple of text for test! </h1>
+        <Button />
+      </div>
+    </>
+  );
+}
+
+export default App;
