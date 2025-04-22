88d1758aaa204ed4fb3a5def7a863e7d9f5df9b6
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2025-04-22 21:49:43 +0200

    fixed the logo, white and black

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,11 @@
+import Logo from '../UI/Logo';
 import Title from '../common/Title';
 
 const Main = () => {
   return (
     <main className='main'>
       <div>
+        <Logo />
         <Title
           variant='page'
           text='Welcome to our coffee shop 👋! And to do me very happy!'
--- src/components/UI/Logo.tsx
+++ src/components/UI/Logo.tsx
@@ -1,10 +1,18 @@
+import imgWhite from '@img/icons/logo.svg?url';
+import imgBlack from '@img/icons/logoBlack.svg?url';
+import clsx from 'clsx';
 import './_styles-ui-component.scss';
 
-import imgPath from '@img/icons/logo.svg?url';
+type LogoColor = 'white' | 'black';
+type LogoProps = {
+  color?: LogoColor;
+  className?: string;
+};
 
-const Logo: React.FC = () => {
+const Logo: React.FC<LogoProps> = ({ color = 'white', className }) => {
+  const imgPath = color === 'white' ? imgWhite : imgBlack;
   return (
-    <div className='logo'>
+    <div className={clsx('logo', `logo--${color}`, className)}>
       <img src={imgPath} alt='Logo' />
     </div>
   );
