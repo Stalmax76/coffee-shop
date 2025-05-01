2fbb68d175be589349e77177bffa3e845af1a7c9
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2025-05-01 13:46:37 +0200

    added footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,31 @@
+import Logo from '@components/UI/Logo';
+import { TitleDecor } from '@components/UI/TitleDecor';
+import './_footer.scss';
+
 const Footer = () => {
-  return <footer className='footer'>Footer</footer>;
+  return (
+    <footer className='footer'>
+      <div className='footer__container'>
+        <div className='footer__content'>
+          <div className='footer__logo'>
+            <Logo color='black' />
+          </div>
+          <div className='footer__items'>
+            <div className='footer__item'>
+              <a href='#'>Coffee house</a>
+            </div>
+            <div className='footer__item'>
+              <a href='#'>Our coffee</a>
+            </div>
+            <div className='footer__item'>
+              <a href='#'>For your pleasure</a>
+            </div>
+          </div>
+        </div>
+        <TitleDecor />
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
