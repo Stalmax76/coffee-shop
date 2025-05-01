bcf5de2758bc3b022222c7445ed640623302aa82
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2025-05-01 08:26:17 +0200

    added header-block

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,27 @@
+import Logo from '../UI/Logo';
+import './_header.scss';
+
 const Header = () => {
   return (
     <header className='header'>
-      <h1>My Application</h1>
+      <div className='header__container'>
+        <div className='header__logo'>
+          <Logo />
+        </div>
+        <nav className='header__nav'>
+          <ul className='header__nav-list'>
+            <li className='header__nav-item'>
+              <a href='#'>Coffee house</a>
+            </li>
+            <li className='header__nav-item'>
+              <a href='#'>Our coffee</a>
+            </li>
+            <li className='header__nav-item'>
+              <a href='#'>For your pleasure</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
