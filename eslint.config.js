fcc0872a53aae297837716de4ab2948b7642d2c8
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2026-05-03 11:11:32 +0200

    added prettier plugins and fixed eslint script

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,45 @@
 import js from '@eslint/js';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    settings: {
-      react: {
-        version: 'detect',
-        jsxRuntime: 'automatic',
+   { ignores: ['dist'] },
+   {
+      files: ['**/*.{ts,tsx}'],
+      languageOptions: {
+         ecmaVersion: 2020,
+         globals: globals.browser,
       },
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    },
-  }
+      plugins: {
+         react,
+         'react-hooks': reactHooks,
+         'react-refresh': reactRefresh,
+         prettier,
+      },
+      extends: [
+         js.configs.recommended,
+         ...tseslint.configs.recommended,
+         react.configs.recommended,
+         prettier.configs.recommended,
+      ],
+      settings: {
+         react: {
+            version: 'detect',
+            jsxRuntime: 'automatic',
+         },
+      },
+      rules: {
+         ...reactHooks.configs.recommended.rules,
+         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+         'prettier/prettier': 'warn',
+         '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+         '@typescript-eslint/no-explicit-any': 'warn',
+         'react/react-in-jsx-scope': 'off',
+         'react/jsx-uses-react': 'off',
+      },
+   },
 );
