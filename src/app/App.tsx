68bf6570b01257146124c638d044481294707227
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2026-05-30 17:07:03 +0200

    change hashrouter on browserRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,16 @@
-// import { BrowserRouter } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+// import { HashRouter } from 'react-router-dom';
 
 import { AppRouter } from '../router';
 
 function App() {
    return (
       // Use Vite's BASE_URL so the router basename matches `base` in vite.config
-      <HashRouter>
+      <BrowserRouter>
          <div className="app">
             <AppRouter />
          </div>
-      </HashRouter>
+      </BrowserRouter>
    );
 }
 
